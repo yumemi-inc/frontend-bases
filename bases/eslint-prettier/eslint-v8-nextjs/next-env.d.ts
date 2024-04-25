1b20d887289d10c7df910539d14b8060b3f2554c
strozw <strozw@users.noreply.github.com>
2024-04-25 12:14:07 +0900

    chore: remove redundant code generated by `next lint`

--- bases/eslint-prettier/eslint-v8-nextjs/next-env.d.ts
+++
@@ -1,6 +0,0 @@
-// このファイルは、`next lint` で生成されたものです
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
